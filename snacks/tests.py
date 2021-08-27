58560727ab05da56a00bbdacd190bc4dd4ecd3d4
Daniel Dills <dills253@gmail.com>
2021-08-26 19:47:22 -0700

    added tests

--- snacks/tests.py
+++ snacks/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+from django.urls import reverse
+from snacks.models import Snack
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class ThingsTests(TestCase):
+    def test_home_page_status_code(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_templete(self):
+        url = reverse('snack_list')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'snack_list.html')
+        self.assertTemplateUsed(response, 'base.html')
+
+    def test_snack_string_representation(self):
+        snack = Snack.objects.create(
+            name = "tacos",
+            purchaser = get_user_model().objects.create_user(
+                username="tester",
+                email="tester@email.com",
+                password="pass"),
+        description="Chicken tacos with a side of rice & beans")
+        self.assertEqual(str(snack), "tacos")
